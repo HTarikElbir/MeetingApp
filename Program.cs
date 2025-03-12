69c514cddbc506985a87d3f37a289e6353ef42f5
HTarık <244035008@ogr.cbu.edu.tr>
2025-03-12 04:02:52 +0300

    Layout, Bootstrap Added

--- Program.cs
+++ Program.cs
@@ -1,13 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
 //For mvc
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews();// Add MVC services to the services container
 
 var app = builder.Build();
+
+app.UseStaticFiles(); // For the wwwroot folder
+
+app.UseRouting(); // For routing
 
 //app.MapDefaultControllerRoute();
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Meeting}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
